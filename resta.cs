91d925430f1122a2ee66158b3e2c4f36dc698488
jose alberto <jose alberto@M1>
2022-09-23 16:54:18 -0600

    Agregar archivos de proyecto.

---
+++ resta.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace resta__constructor
+{
+    class resta
+    {
+        private int num1;
+        private int num2;
+
+
+        public resta(int num1, int num2)
+        {
+            this.num1 = num1;
+            this.num2 = num2;
+        }
+
+
+        public int Resta()
+        {
+            return (num1 - num2);
+        }
+
+    }
+}
